757ff8ee78eb68e6f9564463f0a2bd1f9fc1d21f
Lenar Gatin <lgatin1711@gmail.com>
2025-12-29 17:50:35 +0500

    fix (solution): comply with test awaiting return of pointer to non-const data

--- 02_week/tasks/longest/longest.cpp
+++ 02_week/tasks/longest/longest.cpp
@@ -1,7 +1,7 @@
 #include <cstddef>
 
 
-const char* FindLongestSubsequence(const char* begin, const char* end, size_t& count) {
+char* FindLongestSubsequence(const char* begin, const char* end, size_t& count) {
     if (!begin || !end || begin >= end) {
         count = 0;
         return nullptr;
